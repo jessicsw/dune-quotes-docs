a1cf12aa66a6979e489174a0c3887c05e6ed4a18
jessicsw <jessicaselinewong@gmail.com>
2023-07-02 10:52:28 -0700

    Updated dependencies

---
+++ types.d.ts
@@ -0,0 +1,30 @@
+export type APIReference = {
+  description: string;
+  route: string;
+  hashLink: {
+    id: string;
+    fragment: string;
+  };
+  query?: {
+    title?: boolean;
+    author?: boolean;
+    authorId?: boolean;
+    limit?: boolean;
+    page?: boolean;
+  };
+  example: string;
+};
+
+export namespace Examples {
+  interface ObjectKeys {
+    [key: string]: string | undefined;
+  }
+
+  export interface URL extends ObjectKeys {
+    ["/random"]?: string;
+    ["/quotes"]?: string;
+    ["/quotes/:id"]?: string;
+    ["/books"]?: string;
+    ["/books/:id"]?: string;
+  }
+}
